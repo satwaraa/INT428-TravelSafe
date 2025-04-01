860ce2776575471510b9bb6346cbe0b7a6ab3d52
satwaraa <rvsng028@gmail.com>
2025-04-01 20:43:57 +0530

    removed unwanted await

--- app/api/fetchSafetyData/[location]/route.ts
+++ app/api/fetchSafetyData/[location]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 import * as React from "react";
 export async function GET(req: NextRequest, context: { params: { location: string } }) {
     try {
-        const { location } = await context.params;
+        const { location } = context.params;
         // const { location }: { location: string } = React.use(params);
         if (location) {
             const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
